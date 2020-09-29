d66078d91fadef2056c8f27552fa6ea4395a75c6
jscodebit <soujanya.janapatla@gmail.com>
2020-09-28 21:59:12 -0500

    Initial commit to GitHub

---
+++ Janapatla_Auto_Layout/Janapatla_Auto_Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Janapatla_Auto_Layout
+//
+//  Created by student on 9/28/20.
+//  Copyright © 2020 NorthWest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
